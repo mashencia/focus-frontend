f6b076d8d7d5567a5d54c5456b3c71d1a6d7ae9b
mashencia <mashencia.malic@gmail.com>
2026-01-18 20:39:19 -0500

    add: buttons to start and end break sessions

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,15 @@ export default function App(){
   //active: updated by setActive - focus session currently running
   const [active, setActive]=useState(false);
 
+  //break: updated by setBreak, meaning the user is on break\
+  const [ONbreak, setBreak]=useState(false);
+
   //loading: api request in progress (disables buttons and shows feedback)
   const [loading, setLoading]=useState(false);
 
   //error: storage of backend/network error messages
   const [error, setError]=useState(null);
+
 
 
   //user clicks "Start Focus":
@@ -45,6 +49,52 @@ export default function App(){
       setError(err.message);
     }finally{
       //stoppin the loading visual 
+      setLoading(false);
+    }
+  }
+
+  //user click "Start break"
+  async function startBreak(){
+    try{
+      //Notifies React to load (trigger for re-render)
+      setLoading(true);
+
+      //Previous error messages cleared
+      setError(null);
+
+      //POST req to the backend
+      const res = await api(`/api/focus/break/start/${userId}`, {method: "POST",});
+      console.log("Break session commenced");
+
+      //Marks the break session state active
+      setBreak(true);
+    
+    }catch(err){
+      setError(err.message);
+    }finally{
+      setLoading(false);
+    }
+  }
+
+  //user clicks "End Break"
+async function endBreak(){
+    try{
+      //Notifies React to load (trigger for re-render)
+      setLoading(true);
+
+      //Previous error messages cleared
+      setError(null);
+
+      //POST req to the backend
+      const res = await api(`/api/focus/break/end/${userId}`, {method: "POST",});
+      console.log("Break session commenced");
+
+      //Marks the break session state active
+      setBreak(false);
+    
+    }catch(err){
+      setError(err.message);
+    }finally{
       setLoading(false);
     }
   }
@@ -87,13 +137,26 @@ export default function App(){
         {loading ? "Starting":"Start Focus"}
       </button>
 
+      {/*Start Break Button*/}
+      {active && !ONbreak && (
+        <button onClick={startBreak} disabledled={loading}>
+          Start Break
+        </button>
+      )}
+
+      {/*End Break Button*/}
+      {active && ONBreak &&(
+        <button onClick={endBreak} disabledled={loading}>
+          End Break
+        </button>
+      )}
       {/*End Focus Button*/}
       <button onClick={endFocus} disabledled={loading||!active} style={{marginLeft:10}}>
         {loading ? "Ending":"End Focus"}
       </button>
       {active && <p> Focus session is running</p>}
       {!active && <p> No running focus sessions</p>}
-      
+
       {error && <p style={{color: "red"}}>{error}</p>}
     </div>
   );
